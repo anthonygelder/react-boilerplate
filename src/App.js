1a149e42cf6b3d19523d8ff53a7c80ae696a8c97
Anthony Gelder <anthony.gelder@gmail.com>
2019-10-26 09:59:54 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import { Route } from 'react-router-dom'
+import React, {Component} from 'react'
+import Main from './Main/Main'
+import Sidebar from './Sidebar/Sidebar'
+import './App.css'
 
-function App() {
-  return (
-    <main className='App'>
-      {/* content goes here */}
-    </main>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      game: ''
+    }
+  }
+
+  
+
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>
+            Twitch Game Search
+          </h1>
+        </header>
+        <main className='App'>
+          <Main />
+          <Sidebar />
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
