045b57df385d2460ebec9b60b341ead4c0fa1ca0
Anthony Gelder <anthony.gelder@gmail.com>
2020-07-27 21:33:34 -0700

    readme

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import { Route, Link } from 'react-router-dom'
 import './App.css'
 
 
@@ -10,14 +11,16 @@ class App extends Component {
     }
   }
 
-  
-
   render() {
     return (
       <div>
         <header>
+          <Route path='/' component={} />
         </header>
         <main className='App'>
+          <Link to='/'>
+            Link
+          </Link>
         </main>
       </div>
     );
